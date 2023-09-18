4a5646cb493522f10af4fa851741ca40b246714c
Maksim Ugay <maxim9733012m@gmail.com>
2023-09-19 03:13:31 +0500

    v1.1

---
+++ assets/TvPage-ed1be187.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Dynamic-3443e444.js";import{x as t,y as o,o as s,c,a as n}from"./index-dc055215.js";const r={class:"main"},l={__name:"TvPage",setup(_){const a=t("reload");return o(()=>{a()}),(m,p)=>(s(),c("main",r,[n(e,{type:"tv"})]))}};export{l as default};
